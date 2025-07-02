c96a7d6e54d0f97e68384fe17d1dbb252627b027
ephraim <ephraims.818@gmail.com>
2025-07-02 17:12:56 +0600

    REACT--FUNDAMENTALS--WORKING-WITH-COMPONENTS-PROPS-AND-JSX - 04. JavaScript Logic in Components

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -71,6 +71,18 @@ function Menu() {
 
 function Footer() {
     // return React.createElement("footer", null, "We are currently open!");
+    const hour = new Date().getHours();
+    const openHour = 12;
+    const closeHour = 22;
+    const isOpen = hour >= openHour && hour <= closeHour;
+    console.log(isOpen);
+
+    /* if (hour >= openHour && hour <= closeHour) {
+        alert("We are currently open!");
+    } else {
+        alert("Sorry! We are closed!");
+    } */
+
     return (
         <footer>{new Date().toLocaleTimeString()}We are currently open!</footer>
     );
