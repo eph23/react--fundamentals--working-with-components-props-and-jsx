b3ebd3e44c16065640a52e0c6164737f81f1f011
ephraim <ephraims.818@gmail.com>
2025-07-03 14:53:07 +0600

    REACT--FUNDAMENTALS--WORKING-WITH-COMPONENTS-PROPS-AND-JSX - 05. Styling React Applications

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const pizzaData = [
     {
@@ -48,7 +49,7 @@ const pizzaData = [
 
 function App() {
     return (
-        <div>
+        <div className="container">
             <Header />
             <Menu />
             <Footer />
@@ -57,15 +58,24 @@ function App() {
 }
 
 function Header() {
-    return <h1>Fast React Pizza Co.</h1>;
+    /* const style = {
+        color: "red",
+        fontSize: "48px",
+        textTransform: "uppercase",
+    }; */
+    return (
+        <header className="header">
+            <h1>Fast React Pizza Co.</h1>;
+        </header>
+    );
 }
 
 function Menu() {
     return (
-        <div>
+        <main className="menu">
             <h2>Our Menu</h2>
             <Pizza />
-        </div>
+        </main>
     );
 }
 
@@ -84,7 +94,9 @@ function Footer() {
     } */
 
     return (
-        <footer>{new Date().toLocaleTimeString()}We are currently open!</footer>
+        <footer className="footer">
+            {new Date().toLocaleTimeString()}We are currently open!
+        </footer>
     );
 }
 
@@ -92,7 +104,7 @@ function Pizza() {
     return (
         <div>
             <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
-            <h2>Pizza Spinaci</h2>
+            <h3>Pizza Spinaci</h3>
             <p>Tomato, mozarella, spinach, and ricotta cheese</p>
         </div>
     );
