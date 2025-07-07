662814497f18f105396a8a8672817b7aed607550
ephraim <ephraims.818@gmail.com>
2025-07-07 06:19:30 +0600

    REACT--FUNDAMENTALS--WORKING-WITH-COMPONENTS-PROPS-AND-JSX - Coding Challenge #02

--- profile-card/src/index.js
+++ profile-card/src/index.js
@@ -3,6 +3,39 @@ import ReactDOM, { createRoot } from "react-dom/client";
 
 import "./styles.css";
 
+const skills = [
+    {
+        skill: "HTML+CSS",
+        level: "advanced",
+        color: "#2662EA",
+    },
+    {
+        skill: "JavaScript",
+        level: "advanced",
+        color: "#EFD81D",
+    },
+    {
+        skill: "Web Design",
+        level: "advanced",
+        color: "#C3DCAF",
+    },
+    {
+        skill: "Git and GitHub",
+        level: "intermediate",
+        color: "#E84F33",
+    },
+    {
+        skill: "React",
+        level: "advanced",
+        color: "#60DAFB",
+    },
+    {
+        skill: "Svelte",
+        level: "beginner",
+        color: "#FF3B00",
+    },
+];
+
 function App() {
     return (
         <div className="card">
@@ -40,19 +73,26 @@ function Intro() {
 function SkillList() {
     return (
         <div className="skill-list">
-            <Skill skill="React" emoji="👶" />
-            <Skill skill="JavaScript" emoji="👦" />
-            <Skill skill="HTML" emoji="💪" />
-            <Skill skill="CSS" emoji="💪" />
+            {skills.map((skill) => (
+                <Skill
+                    skill={skill.skill}
+                    color={skill.color}
+                    level={skill.level}
+                />
+            ))}
         </div>
     );
 }
 
-function Skill(props) {
+function Skill({ skill, color, level }) {
     return (
-        <div className="skill">
-            <span>{props.skill}</span>
-            <span>{props.emoji}</span>
+        <div className="skill" style={{ backgroundColor: color }}>
+            <span>{skill}</span>
+            <span>
+                {level === "beginner" && "👶"}
+                {level === "intermediate" && "👍"}
+                {level === "advanced" && "💪"}
+            </span>
         </div>
     );
 }
